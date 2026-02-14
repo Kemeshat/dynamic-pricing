7b65eb30d31ca233381b3d2b62406045a89b5a21
Kemeshat <kemeshat@usf.edu>
2026-02-14 11:25:58 -0500

    Changes made to apply category based discounts

--- cc_4.js
+++ cc_4.js
@@ -7,3 +7,25 @@ const products = [
     { name: "Shampoo",category: "Personal Care", price:8.99,inventory: 15 },
 ];
 
+//Applying category-based discounts
+for (const product of products) {
+    let discount = 0;
+
+    switch (product.name) {
+        case "Ipad":
+            discount= 0.20; // 20% discount for Electronics
+            break;
+        case "Pants":
+            discount = 0.15; // 15% discount for Apparel
+            break;
+        case "Milk":
+            discount = 0.10; // 10% discount for Grocery
+            break;
+        
+            default:
+                discount= 0; // No discount for other categories
+
+    }
+    product.promoPrice = (product.price * (1 - discount)).toFixed(2);
+}
+console.log(products);
