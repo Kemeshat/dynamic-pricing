49ba2823e148491e62421c2eae2d0a25585939b9
Kemeshat <kemeshat@usf.edu>
2026-02-14 10:49:17 -0500

    Product inventory created

---
+++ cc_4.js
@@ -0,0 +1,9 @@
+//Create Product Inventory
+const products = [
+    { name: "Mascara",category: "Cosmetics", price:20.99,inventory: 10 },
+    { name: "Pants", category: "Apparel", price:40.99,inventory: 5 },
+    { name: "Ipad", category: "Electronics", price:960.99,inventory: 3 },
+    { name: "Milk", category: "Grocery", price:3.99,inventory: 20 },
+    { name: "Shampoo",category: "Personal Care", price:8.99,inventory: 15 },
+];
+
